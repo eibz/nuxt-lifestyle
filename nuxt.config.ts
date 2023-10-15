5ea901102d388a18cbbd11f896d9f6370702d067
AtsMultanen <atsmultanen@gmail.com>
2023-10-15 18:04:31 +0100

    API into runtimeconfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,4 +18,10 @@ export default defineNuxtConfig({
             autoprefixer: {},
         },
     },
+    runtimeConfig: {
+        public: {
+            scenesApi: 'https://gist.githubusercontent.com/robwatkiss/09f2461e02d372747dad5fe56ff2251f/raw/b942d9ba21e10889a6cfce639c1a12f6bb2bfa0e/Senior%2520Frontend%2520Developer%2520Task%2520-%2520Sample%2520Lens%2520Guide%2520Data.json',
+            productsApi: 'https://www.sungod.co/products/9150/renegades?pdp=1',
+        }
+    },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -119,9 +119,11 @@
 </template>
 <script setup>
 import { filename } from 'pathe/utils';
+const config = useRuntimeConfig();
+const { productsApi, scenesApi } = config.public;
 
-const products = await useFetch('https://www.sungod.co/products/9150/renegades?pdp=1');
-const scenes = await useFetch('https://gist.githubusercontent.com/robwatkiss/09f2461e02d372747dad5fe56ff2251f/raw/b942d9ba21e10889a6cfce639c1a12f6bb2bfa0e/Senior%2520Frontend%2520Developer%2520Task%2520-%2520Sample%2520Lens%2520Guide%2520Data.json');
+const products = await useFetch(productsApi);
+const scenes = await useFetch(scenesApi);
 
 const scenesObj = JSON.parse(scenes.data.value);
 const { renegades } = products.data.value;
